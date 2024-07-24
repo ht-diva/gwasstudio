3339a41528319398e808278fc29229abb7264f88
Bruno Ariano <bruno.ariano@hnode02.cm.cluster>
2024-07-24 17:08:04 +0200

    add ingestion and s3 configuration

--- gwasstudio/cli/export.py
+++ gwasstudio/cli/export.py
@@ -5,7 +5,7 @@ import pandas as pd
 import tiledbvcf
 from cloup.constraints import mutually_exclusive
 from scipy import stats
-
+import tiledb
 from gwasstudio import logger
 
 help_doc = """
@@ -38,6 +38,17 @@ Exports data from a TileDB-VCF dataset.
     cloup.option("-R", "--regions-file", help="File containing regions (BED format)"),
     cloup.option("-u", "--uri", help="TileDB-VCF dataset URI"),
 )
+@cloup.option_group(
+    "TileDB configurations",
+    cloup.option("-b", "--mem-budget-mb", default=20480, help="The memory budget in MB when ingesting the data"),
+    cloup.option("--aws-access-key-id", default=None, help="aws access key id"),
+    cloup.option("--aws-secret-access-key", default=None, help="aws access key"),
+    cloup.option("--aws-endpoint-override", default="https://storage.fht.org:9021", help="endpoint where to connect"),
+    cloup.option("--aws-use-virtual-addressing", default="false", help="virtual address option"),
+    cloup.option("--aws-scheme", default="https", help="type of scheme used at the endpoint"),
+    cloup.option("--aws-region", default="", help="region where the s3 bucket is located"),
+    cloup.option("--aws-verify-ssl", default="false", help="if ssl verfication is needed")
+)
 @click.pass_context
 def export(
     ctx,
@@ -52,13 +63,35 @@ def export(
     samples_file,
     samples,
     uri,
+    aws_access_key_id,
+    aws_secret_access_key,
+    aws_endpoint_override,
+    aws_use_virtual_addressing,
+    aws_scheme,
+    aws_region,
+    aws_verify_ssl
+
 ):
     if ctx.obj["DISTRIBUTE"]:
         pass
     else:
         _attrs = [a.strip() for a in attrs.split(",")]
-        cfg = tiledbvcf.ReadConfig(memory_budget_mb=mem_budget_mb)
-        ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
+        cfg = tiledbvcf.ReadConfig(
+            memory_budget_mb=mem_budget_mb)
+        tiledb.default_ctx({
+        "vfs.s3.scheme": aws_scheme,
+        "vfs.s3.region": aws_region,
+        "vfs.s3.endpoint_override": aws_endpoint_override,
+        "vfs.s3.use_virtual_addressing": aws_use_virtual_addressing,
+        "vfs.s3.aws_secret_access_key": aws_secret_access_key,
+        "vfs.s3.aws_access_key_id": aws_access_key_id,
+        "vfs.s3.verify_ssl": aws_verify_ssl,
+        })
+
+        cfg = tiledb.Config()
+        read_cfg = tiledbvcf.ReadConfig(tiledb_config=cfg)
+
+        ds = tiledbvcf.Dataset(uri, mode="r", cfg=read_cfg)
 
         frames = []
         results = {}
--- gwasstudio/cli/query.py
+++ gwasstudio/cli/query.py
@@ -20,8 +20,31 @@ Get some information about your dataset, such as the sample names, the attribute
     ),
     cloup.option("-u", "--uri", help="TileDB-VCF dataset URI"),
 )
+@cloup.option_group(
+    "TileDB configurations",
+    cloup.option("-b", "--mem-budget-mb", default=20480, help="The memory budget in MB when ingesting the data"),
+    cloup.option("-t", "--threads", default=16, help="The number fo threwads used for ingestion"),
+    cloup.option("--aws-access-key-id", default=None, help="aws access key id"),
+    cloup.option("--aws-secret-access-key", default=None, help="aws access key"),
+    cloup.option("--aws-endpoint-override", default="https://storage.fht.org:9021", help="endpoint where to connect"),
+    cloup.option("--aws-use-virtual-addressing", default="false", help="virtual address option"),
+    cloup.option("--aws-scheme", default="https", help="type of scheme used at the endpoint"),
+    cloup.option("--aws-region", default="", help="region where the s3 bucket is located"),
+    cloup.option("--aws-verify-ssl", default="false", help="if ssl verfication is needed")
+    )
 @click.pass_context
-def query(ctx, mem_budget_mb, information, uri):
+def query(
+    ctx, 
+    mem_budget_mb, 
+    aws_access_key_id,
+    aws_secret_access_key,
+    aws_endpoint_override,
+    aws_use_virtual_addressing,
+    aws_scheme,
+    aws_region,
+    aws_verify_ssl,
+    information, 
+    uri):
     cfg = tiledbvcf.ReadConfig(memory_budget_mb=mem_budget_mb)
     ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
     if information == "samples":
