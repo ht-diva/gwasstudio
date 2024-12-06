27ebb4e313a602444cd33a121e59cdd9274b2026
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2024-12-06 10:46:18 +0100

    update meta-ingest

--- gwasstudio/cli/metadata/ingest.py
+++ gwasstudio/cli/metadata/ingest.py
@@ -17,14 +17,32 @@ Ingest metadata into a MongoDB collection.
 @cloup.option_group(
     "Ingestion options",
     cloup.option(
-        "--data-path",
+        "--filepath",
         default=None,
-        help="Path to dataset directory",
+        help="Path to the file to ingest",
     ),
+    cloup.option(
+        "--pathlist",
+        default=None,
+        help="Path to the file containing a list of files to ingest. One path per line",
+    ),
+    cloup.option("--annotation-file", default=None, help="File path with the annotation for the trait's feature"),
 )
-def meta_ingest(data_path):
-    file_list = sorted(Path(data_path).rglob("*.txt.gz"))
-    project = "DECODE_largescaleplasma2023"
+def meta_ingest(filepath, pathlist, annotation_file):
+    input_file_list = []
+    if filepath:
+        input_file_list.append(filepath)
+    elif pathlist:
+        with open(pathlist, "r") as fp:
+            for line in fp:
+                input_file_list.append(line.strip())
+    else:
+        logger.error("No input provided")
+        exit()
+
+    # file_list = sorted(Path(data_path).rglob("*.txt.gz"))
+    project = "DECODE"
+    # study = "largescaleplasma2023"
     population = "Icelandic"
     category = "pQTL"
     build = "GRCh38"
@@ -35,18 +53,23 @@ def meta_ingest(data_path):
         "version": "v4",
         "normalization": "raw",
     }
-    logger.info("{} documents to ingest".format(len(file_list)))
-    for path in file_list:
+    tissue = "Plasma"
+    annotation_columns = ["SeqId", "UniProt ID", "Ensembl Gene ID"]
+    df_annotation = pd.read_csv(Path(annotation_file), usecols=annotation_columns, sep=";")
+    renamed_columns = {k: k.replace(" ", "_").lower() for k in annotation_columns}
+    df_annotation = df_annotation.rename(columns=renamed_columns)
+
+    logger.info("{} documents to ingest".format(len(input_file_list)))
+    for path in input_file_list:
         df = pd.read_csv(Path(path), compression="gzip", nrows=1, usecols=["N"], sep="\t")
         total_samples = int(df.loc[0, "N"])
         file_hash = compute_sha256(fpath=path)
         basename = Path(path).name.split("_")[:-1]
-        seqid = "_".join([basename[2], basename[3]])
-        genid = basename[4]
-        protid = "_".join(basename[5:])
-        tissue = "Plasma"
+        seqid = "-".join([basename[2], basename[3]])
+        gene_ids = df_annotation[df_annotation["seqid"] == seqid]["ensembl_gene_id"].item()
+        protein_ids = df_annotation[df_annotation["seqid"] == seqid]["uniprot_id"].item()
         trait_desc = {
-            "feature": {"seqid": seqid, "genid": genid, "protid": protid},
+            "feature": {"seqid": seqid, "gene_ids": gene_ids, "protein_ids": protein_ids},
             "tissue": tissue,
             "platform": platform,
         }
