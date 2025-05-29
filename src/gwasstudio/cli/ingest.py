e976db39515ed636ab6eef92f77eaa52c9b9c8cf
Bruno Ariano <bruno.ariano@fht.org>
2025-05-29 12:50:51 +0200

    other rebase

--- src/gwasstudio/cli/ingest.py
+++ src/gwasstudio/cli/ingest.py
@@ -6,8 +6,10 @@ from dask import delayed, compute
 
 from gwasstudio import logger
 from gwasstudio.dask_client import dask_deployment_types
-from gwasstudio.utils import create_tiledb_schema, parse_uri, process_and_ingest
-from gwasstudio.utils.cfg import get_tiledb_config, get_dask_batch_size, get_dask_deployment
+from gwasstudio.utils import create_tiledb_schema, parse_uri, process_and_ingest, check_file_exists
+from gwasstudio.utils.cfg import get_tiledb_config, get_dask_batch_size, get_dask_deployment, get_mongo_uri
+from gwasstudio.utils.metadata import load_metadata, ingest_metadata
+from gwasstudio.utils.mongo_manager import manage_mongo
 from gwasstudio.utils.s3 import does_uri_path_exist
 from dask import delayed, compute
 import pandas as pd
@@ -21,9 +23,14 @@ Ingest data in a TileDB-unified dataset.
 @cloup.option_group(
     "Ingestion options",
     cloup.option(
-        "--multiple-input",
-        default=None,
-        help="Path to the file containing a list of files to ingest. One path per line",
+        "--file-path",
+        required=True,
+        help="Path to the tabular file containing details for the ingestion",
+    ),
+    cloup.option(
+        "--delimiter",
+        default="\t",
+        help="Character or regex pattern to treat as the delimiter.",
     ),
     cloup.option(
         "--uri",
@@ -32,14 +39,41 @@ Ingest data in a TileDB-unified dataset.
     ),
 )
 @click.pass_context
-def ingest(ctx, multiple_input, uri):
-    """Ingest data in a TileDB-unified dataset."""
-    # Check if the file exists
-    input_file_list = pd.read_csv(multiple_input, header = None)
-    input_file_list = input_file_list[0].tolist()
-    if not check_file_exists(input_file_list, logger):
-        exit(1)
-    # Parse the uri
+def ingest(ctx, file_path, delimiter, uri):
+    """
+    Ingest data into a TileDB-unified dataset.
+
+    This function reads metadata from a specified file, validates the required columns,
+    and processes the metadata for ingestion into a MongoDB collection, and data files for ingestion into a TileDB dataset. It supports both S3 and
+    local file system storage.
+
+    Args:
+        ctx (click.Context): The click context.
+        file_path (str): Path to the tabular file containing details for the ingestion.
+        delimiter (str): Character or regex pattern to treat as the delimiter.
+        uri (str): Destination path where to store the tiledb dataset.
+
+    Raises:
+        ValueError: If the file does not exist or required columns are missing.
+    """
+    if not check_file_exists(file_path, logger=logger):
+        raise ValueError(f"File {file_path} does not exist")
+    if not uri:
+        raise ValueError("URI is required")
+
+    df = load_metadata(Path(file_path), delimiter)
+    required_columns = ["project", "study", "file_path", "category"]
+    missing_cols = set(required_columns) - set(df.columns)
+    if missing_cols:
+        raise ValueError(f"Missing column(s) in the input file: {', '.join(missing_cols)}")
+
+    with manage_mongo(ctx):
+        mongo_uri = get_mongo_uri(ctx)
+        ingest_metadata(df, mongo_uri)
+
+    input_file_list = df["file_path"].tolist()
+    logger.info("Starting data ingestion: {} file to process".format(len(input_file_list)))
+
     scheme, netloc, path = parse_uri(uri)
     if scheme == "s3":
         ingest_to_s3(ctx, input_file_list, uri)
@@ -59,6 +93,17 @@ def check_file_exists(input_file_list, logger):
     return True
 
 def ingest_to_s3(ctx, input_file_list, uri):
+    """
+    Ingest data into an S3-based TileDB dataset.
+
+    This function processes a list of input files and ingests them into a TileDB dataset
+    stored in an S3 bucket. It supports batch processing using Dask.
+
+    Args:
+        ctx (click.Context): The click context.
+        input_file_list (list): List of file paths to be ingested.
+        uri (str): Destination path where to store the tiledb dataset in S3.
+    """
     cfg = get_tiledb_config(ctx)
     if not does_uri_path_exist(uri, cfg):
         logger.info("Creating TileDB schema")
@@ -86,6 +131,17 @@ def ingest_to_s3(ctx, input_file_list, uri):
 
 
 def ingest_to_fs(ctx, input_file_list, uri):
+    """
+    Ingest data into a local file system-based TileDB dataset.
+
+    This function processes a list of input files and ingests them into a TileDB dataset
+    stored in the local file system. It supports batch processing using Dask.
+
+    Args:
+        ctx (click.Context): The click context.
+        input_file_list (list): List of file paths to be ingested.
+        uri (str): Destination path where to store the tiledb dataset in the local file system.
+    """
     _, __, path = parse_uri(uri)
     batch_size = ctx.obj["dask"]["batch_size"]
     if not Path(path).exists():
