8e1b796daa1b2e4541705656f322b6b1088a5ef3
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-06-05 21:12:44 +0200

    chore: Simplify cli_test.sh script by introducing a run_command function
    The cli_test.sh script has been refactored to use a new run_command function, which encapsulates the running of gwasstudio commands with a description. This simplifies the code and reduces repetition.
    
    This change is necessary to improve maintainability and readability of the script. The new function makes it easier to add or remove commands without modifying the surrounding logic.
    
    The run_command function takes two arguments: a description and a command to be executed. It echoes the description, runs the command, and provides feedback on the execution outcome.

--- scripts/cli_test.sh
+++ scripts/cli_test.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Define the data and test directory variables
-GWASSTUDIO_DATA_DIR=`gwasstudio info | grep "data dir:" | awk -F': ' '{print $2}'`
+GWASSTUDIO_DATA_DIR=$(gwasstudio info | grep "data dir:" | awk -F': ' '{print $2}')
 TEST_DIR="../scripts/test_results/cli"
 
 # Clean up existing mongodata and test directories
@@ -19,46 +19,34 @@ rm -rf "${TEST_DIR}"
 # Create the test directory structure
 mkdir -p "${TEST_DIR}"
 
+# Function to run a command with a description
+run_command() {
+  local description=$1
+  local cmd=$2
+  echo "${description}"
+  echo "Running command: ${cmd}"
+  eval ${cmd}
+}
+
 # Ingest data
-echo "Ingesting data..."
-INGEST_CMD="gwasstudio --stdout ingest --file-path metadata_ukb_d_sampled.tsv --uri ${TEST_DIR}/destination"
-echo "Running command: ${INGEST_CMD}"
-$INGEST_CMD
+run_command "Ingesting data..." "gwasstudio --stdout ingest --file-path metadata_ukb_d_sampled.tsv --uri ${TEST_DIR}/destination"
 
 # Query data
-echo "Querying data..."
-QUERY_CMD="gwasstudio --stdout meta-query --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_query"
-echo "Running command: ${QUERY_CMD}"
-$QUERY_CMD
+run_command "Querying data..." "gwasstudio --stdout meta-query --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_query"
 
-# Query data
-echo "Querying data by trait description..."
-QUERY_CMD_BY_TRAIT_DESC="gwasstudio --stdout meta-query --search-file search_ukb_d_filter_by_trait_desc.txt --output-prefix ${TEST_DIR}/ukb_d_query_by_trait_desc"
-echo "Running command: ${QUERY_CMD_BY_TRAIT_DESC}"
-$QUERY_CMD_BY_TRAIT_DESC
+# Query data by trait description
+run_command "Querying data by trait description..." "gwasstudio --stdout meta-query --search-file search_ukb_d_filter_by_trait_desc.txt --output-prefix ${TEST_DIR}/ukb_d_query_by_trait_desc"
 
 # Export data
-echo "Exporting data..."
-EXPORT_CMD="gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_export --uri ${TEST_DIR}/destination"
-echo "Running command: ${EXPORT_CMD}"
-$EXPORT_CMD
+run_command "Exporting data..." "gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_export --uri ${TEST_DIR}/destination"
 
 # Regions filtering
-echo "Regions filtering..."
-RF_CMD="gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_regions_filtering --uri ${TEST_DIR}/destination --get-regions regions_query.tsv"
-echo "Running command: ${RF_CMD}"
-$RF_CMD
+run_command "Regions filtering..." "gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_regions_filtering --uri ${TEST_DIR}/destination --get-regions regions_query.tsv"
 
 # Hapmap3 SNPs filtering
-echo "SNPs filtering..."
-SF_CMD="gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_snps_filtering --uri ${TEST_DIR}/destination --snp-list-file hapmap3/hapmap3_snps.csv"
-echo "Running command: ${SF_CMD}"
-$SF_CMD
-
+run_command "SNPs filtering..." "gwasstudio --stdout --local-workers 4 export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_snps_filtering --uri ${TEST_DIR}/destination --snp-list-file hapmap3/hapmap3_snps.csv"
 
 # Locusbreaker
-echo "Locusbreaker..."
-LB_CMD="gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_locusbreaker --uri ${TEST_DIR}/destination --locusbreaker"
-$LB_CMD
+run_command "Locusbreaker..." "gwasstudio --stdout export --search-file search_ukb_d.txt --output-prefix ${TEST_DIR}/ukb_d_locusbreaker --uri ${TEST_DIR}/destination --locusbreaker"
 
 echo "Results are available in ${TEST_DIR}"
