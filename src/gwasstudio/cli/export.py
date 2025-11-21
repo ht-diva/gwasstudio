9ebb9fa8aa90f92dcdc967e865dfb67346e19ce9
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2025-11-21 15:25:13 +0100

    feat: Enhance BED file processing
    Improved handling of BED files by adding support for non-numeric chromosome values and SNP lists. The updates include:
    
    - Removing 'chr' prefix from chromosome columns.
    - Converting X/Y chromosomes to 23/24.
    - Filtering out rows with non-numeric chromosome values.

--- src/gwasstudio/cli/export.py
+++ src/gwasstudio/cli/export.py
@@ -87,20 +87,51 @@ def _process_function_tasks(
             return None
         try:
             # Try BED format
-            df = pd.read_csv(fp, sep="\t", header=None, names=["CHR", "START", "END"])
+            df = pd.read_csv(
+                fp,
+                sep="\t",
+                header=None,
+                names=["CHR", "START", "END"],
+                usecols=range(3),
+                dtype={"CHR": str, "START": int, "END": int},
+            )
+
+            # Remove 'chr' prefix and convert X/Y to 23/24
+            df.loc[:, "CHR"] = df["CHR"].str.replace("chr", "", case=False)
+            df.loc[:, "CHR"] = df["CHR"].replace({"X": 23, "Y": 24}).astype(str)
+
+            count_row_before = df.shape[0]
+            df = df[df["CHR"].str.isnumeric()]
+            row_diff = count_row_before - df.shape[0]
+            if row_diff > 0:
+                logger.warning(f"Removed {row_diff} rows with non-numeric CHR values.")
+
             df.loc[:, "CHR"] = df["CHR"].astype(int)
+
             return df
-        except Exception:
+        except Exception as e:
+            logger.debug(f"Trying to use BED format: {e}")
             pass
         try:
             # Try SNP list and convert to BED format
             df = pd.read_csv(fp, usecols=["CHR", "POS"], dtype={"CHR": str, "POS": int})
+
+            # Remove 'chr' prefix and convert X/Y to 23/24
+            df.loc[:, "CHR"] = df["CHR"].str.replace("chr", "", case=False)
+            df.loc[:, "CHR"] = df["CHR"].replace({"X": 23, "Y": 24}).astype(str)
+
+            count_row_before = df.shape[0]
             df = df[df["CHR"].str.isnumeric()]
+            row_diff = count_row_before - df.shape[0]
+            if row_diff > 0:
+                logger.warning(f"Removed {row_diff} rows with non-numeric CHR values.")
+
             df.loc[:, "CHR"] = df["CHR"].astype(int)
             df = df.rename(columns={"POS": "START"})
-            df["END"] = df["START"] + 1
-            return df[["CHR", "START", "END"]]
-        except Exception:
+            df.loc[:, "END"] = df["START"] + 1
+            return df
+        except Exception as e:
+            logger.debug(f"Trying to use SNP list format: {e}")
             raise ValueError(f"--get_regions_snps file '{fp}' should be in BED format or a SNP list (CHR,POS)")
 
     # Wrapper that opens the array locally and forwards the call.
@@ -136,11 +167,6 @@ def _process_function_tasks(
 
     if regions_snps:
         kwargs["regions_snps"] = delayed(_read_to_bed)(regions_snps)
-
-    # if isinstance(group, pd.Series):
-    #     trait_id_list = group.to_list()
-    # else:
-    #     trait_id_list = group["data_id"].to_list()
 
     trait_id_list = group["data_id"].tolist() if not isinstance(group, pd.Series) else group.tolist()
 
